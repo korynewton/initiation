fc461b2e0f8ac9c6c557a2576317db59f7f4db7c
Kory Newton <45835901+korynewton@users.noreply.github.com>
2020-06-26 11:11:48 -0700

    top level App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SignInPage from './Pages/SignInPage';
+import DashboardPage from './Pages/DashboardPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+interface ParentState {
+  loggedIn: boolean;
+  token: null | string;
+}
 
 function App() {
+  const [loggedInData, setLoggedInData] = useState<ParentState>({
+    loggedIn: false,
+    token: null,
+  });
+
+  const handleSuccessfulLogIn = (token: string) => {
+    setLoggedInData({
+      loggedIn: true,
+      token,
+    });
+  };
+
+  const { loggedIn, token } = loggedInData;
   return (
-    <div>
-      <h2>working</h2>
+    // if logged in show Dashboard, else show sign in page
+    <div className="p-4">
+      {loggedIn ? (
+        <DashboardPage token={token} />
+      ) : (
+        <SignInPage handleSuccessfulLogIn={handleSuccessfulLogIn} />
+      )}
     </div>
   );
 }
